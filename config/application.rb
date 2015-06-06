41138162a684b3237c369887a9942bbafae1d5cd
Víctor Viruete <victor@diacode.com>
2015-06-06 12:09:11 +0200

    Removed a couple of warnings from rails 4.1 to 4.2 migration

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,9 @@ module Smithers
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :es
+    
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
 
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
