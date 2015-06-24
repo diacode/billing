092bf1eb2b050bdb910d79a74c97a8a5dfb937fc
Víctor Viruete <victor@diacode.com>
2015-06-24 19:30:47 +0200

    Minor balance improvement in dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < BaseController
   def index
-    @balance = BankRecord.limit(1).pluck(:balance).first
     @monthly_income = BankRecord.current_month.income.sum(:amount)
     @monthly_expenses = BankRecord.current_month.expenses.sum(:amount)
     @bank_records = BankRecord.limit(5)
     @invoices = Invoice.limit(5)
     @pending = Invoice.pending.to_a.sum(&:total)
+    @balance = @bank_records.any? ? @bank_records.first.balance : 0
   end
 end
