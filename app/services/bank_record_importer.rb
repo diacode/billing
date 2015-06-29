c6d6b3e9e7015b168e547de50770a50e1358b02b
Víctor Viruete <victor@diacode.com>
2015-06-29 17:22:17 +0200

    BankRecordImporter implemented

--- app/services/bank_record_importer.rb
+++ app/services/bank_record_importer.rb
@@ -1,3 +1,7 @@
+# TODO: Implement specific account importing. Right now it only imports transactions from
+# the first bank account available in our bank. It should work like that if there is no account
+# id specified in billing.yml config file. If it's explicitly specified then it should import 
+# transactions from that account
 class BankRecordImporter
   def initialize
     username = ::BillingConfig['bank_account']['username'].to_s
@@ -15,9 +19,35 @@ class BankRecordImporter
   # to get more bank records older than 2 years.
   def import_all
     account = @bank.accounts.first
-    account.fetch_transactions(start_date: Date.today - 2.years, end_date: Date.today)
+    transactions = account.fetch_transactions(start_date: Date.today - 2.years, end_date: Date.today)
+    transactions.reverse!
+    transactions.each do |t|
+      BankRecord.create(
+        subject: t.description,
+        amount: t.amount,
+        balance: t.balance,
+        operation_at: t.effective_date,
+        value_at: t.effective_date,
+        transaction_id: t.id
+      )
+    end
   end
 
   def import_last
+    account = @bank.accounts.first
+    transactions = account.fetch_transactions(start_date: Date.today - 2.days, end_date: Date.today)
+    transactions.reverse!
+    transactions.each do |t|
+      unless BankRecord.exists?(transaction_id: t.id)
+        BankRecord.create(
+          subject: t.description,
+          amount: t.amount,
+          balance: t.balance,
+          operation_at: t.effective_date,
+          value_at: t.effective_date,
+          transaction_id: t.id
+        )
+      end
+    end
   end
 end
