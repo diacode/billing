5237e8e8cfa690bee423168a46bd51480bcf6608
Víctor Viruete <victor@diacode.com>
2015-06-29 15:43:11 +0200

    Added transaction id column to bank records

--- spec/factories/bank_records.rb
+++ spec/factories/bank_records.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: bank_records
+#
+#  id             :integer          not null, primary key
+#  subject        :string
+#  amount         :decimal(11, 2)
+#  balance        :decimal(11, 2)
+#  operation_at   :date
+#  value_at       :date
+#  created_at     :datetime
+#  updated_at     :datetime
+#  transaction_id :string
+#
+
 FactoryGirl.define do
   factory :bank_record do
     subject Faker::Name.title
